cdc121fb776f525036e783b22883ee18c5cd1f9a
Tony ElHabr <anthonyelhabr@gmail.com>
2021-12-05 20:34:05 -0600

    rosters

---
+++ R/url.R
@@ -0,0 +1,41 @@
+#' Check if URL exists
+#'
+#' @param x a single URL
+#' @param non_2xx_return_value what to do if the site exists but the
+#'        HTTP status code is not in the `2xx` range. Default is to return `FALSE`.
+#' @param quiet if not `FALSE`, then every time the `non_2xx_return_value` condition
+#'        arises a warning message will be displayed. Default is `FALSE`.
+#' @param ... other params (`timeout()` would be a good one) passed directly
+#'        to `httr::HEAD()` and/or `httr::GET()`
+#' @source https://stackoverflow.com/questions/52911812/check-if-url-exists-in-r
+url_exists <- function(x, non_2xx_return_value = FALSE, quiet = FALSE,...) {
+  
+  suppressPackageStartupMessages({
+    require("httr", quietly = FALSE, warn.conflicts = FALSE)
+  })
+
+  sHEAD <- safely(httr::HEAD)
+  sGET <- safely(httr::GET)
+  
+  # Try HEAD first since it's lightweight
+  res <- sHEAD(x, ...)
+  
+  if (is.null(res$result) || 
+      ((httr::status_code(res$result) %/% 200) != 1)) {
+    
+    res <- sGET(x, ...)
+    
+    if (is.null(res$result)) return(NA) # or whatever you want to return on "hard" errors
+    
+    if (((httr::status_code(res$result) %/% 200) != 1)) {
+      if (!quiet) warning(sprintf("Requests for [%s] responded but without an HTTP status code in the 200-299 range", x))
+      return(non_2xx_return_value)
+    }
+    
+    return(TRUE)
+    
+  } else {
+    return(TRUE)
+  }
+  
+}
--- data-raw/rosters.R
+++ data-raw/rosters.R
@@ -1,102 +1,111 @@
 
-
-library(tidyverse)
+library(dplyr)
+library(purrr)
+library(stringr)
 library(rvest)
+library(janitor)
+library(usethis)
+library(rlang)
+library(tidyr)
+library(lubridate)
 
-# pluck_element <- function(players, .css) {
-#   players %>% html_elements(.css) %>% html_text2()
-# }
-# pluck_date_element <- function(players) {
-#   players %>% 
-#     html_elements('.Date') %>% 
-#     html_elements('div.Date') %>% 
-#     html_text2()
-# }
+get_team_url <- function(team_name) {
+  case_when(
+    team_name == 'KC Pioneers' ~ 'Kansas_City_Pioneers'
+    team_name == 'Spacestation' ~ 'Spacestation_Gaming',
+    TRUE ~ str_replace_all(team_name, ' ', '_')
+  )
+  sprintf(
+    'https://liquipedia.net/halo/%s', team_name
+  )
+}
+
+data('all_teams', package = 'hcs')
+
+team_urls <- all_teams %>% 
+  mutate(team_url = get_team_url(team))
 
 scrape_roster <- function(team_url) {
-  page <- team_urls[2] %>% read_html()
+  team <- team_url %>% str_remove_all('^.*\\/') %>% str_replace_all('_', ' ')
+  cat(
+    sprintf('Scraping roster for %s.', team),
+    sep = '\n'
+  )
+
+  team_url_exists <- team_url %>% url_exists()
+  if(!team_url_exists) {
+    return(tibble())
+  }
+  page <- team_url %>% read_html()
   table_elements <- page %>% html_elements('.wikitable.wikitable-striped.roster-card')
   n_tables <- length(table_elements)
   scrape_method <- 'roster_card_wikitable'
+  
   if(n_tables == 0) {
     # it may be in an "old" format
     # there could be more than just the table(s) for rosters
     table_elements <- page %>% html_elements('.wikitable.wikitable-striped.sortable')
     n_tables <- length(table_elements)
     scrape_method <- 'generic_wikitable'
+    stopifnot(
+      "Number of tables must be greater than 1" = n_tables >= 1
+    )
   } else {
     
-    ## one for active and possibly one for former
-    stopifnot(n_tables >= 1 & n_tables <= 2)
-    
-    # pluck_table_element <- function(i) {
-    #   table_elements[i] %>% html_elements('.Player')
-    # }
-    # 
-    # active_player_elements <- pluck_table_element(1)
-    # 
-    # players <- tibble(
-    #   id = active_player_elements %>% pluck_element('.ID'),
-    #   name = active_player_elements %>% pluck_element('.Name'),
-    #   join_date = active_player_elements %>% pluck_date_element()
-    # ) %>% 
-    #   mutate(
-    #     status = 'active',
-    #     leave_date = NA_character_
-    #   )
-    # 
-    # if(n_tables == 2) {
-    #   former_player_elements <- pluck_table_element(2)
-    #   dates <- former_player_elements %>% pluck_date_element()
-    #   n_dates <- length(dates)
-    #   idx_join <- seq(1, n_dates, by = 2)
-    #   idx_leave <- seq(2, n_dates, by = 2)
-    #   former_players <- tibble(
-    #     id = former_player_elements %>% pluck_element('.ID'),
-    #     name = former_player_elements %>% pluck_element('.Name'),
-    #     join_date = dates[idx_join],
-    #     leave_date = dates[idx_leave]
-    #   ) %>% 
-    #     mutate(status = 'former')
-    #   
-    #   players <- bind_rows(
-    #     players,
-    #     former_players
+    # if(!(n_tables >= 1 & n_tables <= 2)) {
+    #   abort(
+    #     sprintf("Number of tables must be between 1 and 2, not %s", n_tables)
     #   )
     # }
-    # players
   }
   
   pluck_table <- function(i) {
-    table_init <- table_elements[i] %>% html_table() %>% pluck(1) %>% janitor::remove_empty(which = 'cols')
+    table_init <- table_elements[i] %>% 
+      html_table() %>% 
+      pluck(1) %>% 
+      remove_empty(which = 'cols')
+    old_names <- table_init %>% names()
     first_row <- table_init[1, ] %>% c() %>% unname() %>% unlist()
     tb <- table_init %>% 
       set_names(first_row) %>% 
       slice(c(2:n())) %>% 
-      janitor::clean_names()
+      clean_names() %>% 
+      mutate(
+        table_name = old_names[1]
+      )
   }
   
-  1:pmin(n_tables, 2) %>% 
-    map_dfr(pluck_table)
+  
+  res <- 1:pmin(n_tables, 2) %>% 
+    map_dfr(pluck_table) %>% 
+    mutate(scrape_method = !!scrape_method)
+
+  res
 }
 
-get_team_url <- function(team_name) {
-  sprintf(
-    'https://liquipedia.net/halo/%s', str_replace_all(team_name, ' ', '_')
-  )
-}
-
-team_urls <- 
-  tibble(
-    team = 
-      c(
-        'EUnited',
-        'Kansas City Pioneers',
-        'OpTic Gaming',
-        'Status Quo'
-      )
-  ) %>% 
-  mutate(team_url = get_team_url(team))
 possibly_scrape_roster <- possibly(scrape_roster, otherwise = tibble())
 rosters <- team_urls %>% 
-  map_dfr(possibly_scrape_roster)
+  # head(3) %>% 
+  head(10) %>% 
+  mutate(
+    roster = map(team_url, possibly_scrape_roster)
+  )
+rosters
+clean_date <- function(x, which) {
+  str_remove_all(x, sprintf('^%s\\sDate[:]\\s|\\[[0-9]+\\]$', str_to_title(which))) %>% ymd()
+}
+
+all_rosters <- rosters %>% 
+  select(team, roster) %>% 
+  unnest(roster) %>% 
+  transmute(
+    team,
+    id,
+    status = table_name %>% str_replace_all('(^.*)(\\s.*$)', '\\1') %>% tolower(),
+    across(name, ~str_remove_all(.x, '^\\(|\\)')),
+    position,
+    across(join_date, ~clean_date(.x, 'join')),
+    across(leave_date, ~clean_date(.x, 'leave'))
+  )
+all_rosters %>% count(team, sort = TRUE)
+  
