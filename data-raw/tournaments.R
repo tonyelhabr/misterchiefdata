8c0bbc5ce0eeebcbb08a9d89bd3be19796471c81
Tony ElHabr <anthonyelhabr@gmail.com>
2021-12-16 09:12:03 -0600

    cleaner tourneys

--- data-raw/tournaments.R
+++ data-raw/tournaments.R
@@ -2,14 +2,69 @@
 library(tidyverse)
 library(lubridate)
 library(rvest)
+library(usethis)
 
 scrape_time <- now()
+
+.coalesce_tourney_date <- function(date) {
+  date %>% 
+    strptime('%b %d, %Y', tz = 'UTC') %>% 
+    ymd()
+}
+
+.str_replace_tourney_date <- function(date, which = c('start', 'end')) {
+  
+  has_dash <- str_detect(date, '[-]')
+  if(!has_dash) {
+    return(date)
+  }
+  
+  match.arg(which)
+  replacement <- switch(
+    which,
+    'start' = '\\1 \\2, \\5',
+    'end' = '\\3 \\4, \\5'
+  )
+  
+  # date <- "Apr 29 - May 1, 2022"
+  res <- str_replace(date, '^([A-Z][a-z]+)\\s([0-9]+)\\s[-]\\s([A-Z][a-z]+)\\s([0-9]+)[,]\\s(20[012][0-9]$)', replacement)
+  if(!is.na(res) & res != date) {
+    return(res)
+  }
+  
+  replacement <- switch(
+    which,
+    'start' = '\\1 \\2, \\4',
+    'end' = '\\1 \\3, \\4'
+  )
+  
+  # Something like Sep 25 - 27, 2022
+  # date <- "Sep 23 - 25, 2022"
+  res <- str_replace(date, '^([A-Z][a-z]+)\\s([0-9]+)\\s[-]\\s([0-9]+)[,]\\s(20[012][0-9]$)', replacement)
+  if(!is.na(res) & res != date) {
+    return(res)
+  }
+
+  if(is.na(res)) {
+    rlang::warn(
+      sprintf('Couldn\'t coalesce `date = %s` with `which = %s`.', date, which)
+    )
+  }
+  res
+}
+
+.clean_abbrv <- function(x) {
+  y <- str_remove(x, '^TBD$')
+  ifelse(y == '', NA_character_, y)
+}
 
 scrape_tournament <- function(url) {
   cat(
     sprintf('Scraping %s.', basename(url)),
     sep = '\n'
   )
+  
+  ## scrape ----
   page <- url %>% read_html()
   tourney_nodes <- page %>% html_elements('.divTable.table-full-width.tournament-card')
   name_nodes <- tourney_nodes %>% html_elements('.divCell.Tournament.Header') %>% html_elements('b')
@@ -24,7 +79,7 @@ scrape_tournament <- function(url) {
   participants <- extract_text('EventDetails-Right-40.Header')
   first_place_abbrvs <- extract_text('Placement.FirstPlace')
   second_place_abbrvs <- extract_text('Placement.SecondPlace')
-
+  
   extract_title <- function(x) {
     els <- tourney_nodes %>% 
       html_elements(sprintf('.divCell.Placement.%sPlace', x))
@@ -37,26 +92,52 @@ scrape_tournament <- function(url) {
             return(NA_character_)
           }
           el %>%
-            html_attr('title') %>% 
-            str_remove(' \\(page does not exist\\)')
+            html_attr('title')
         }
       )
   }
   first_place_names <- extract_title('First')
   second_place_names <- extract_title('Second')
   
+  regions <- tourney_nodes %>% 
+    html_elements('.divCell.EventDetails-Left-60.Header') %>% 
+    html_elements('.flag > a') %>% 
+    html_attr('title')
+  
+  ## clean ----
+  start_dates <- dates %>% map_chr(~.str_replace_tourney_date(.x, 'start')) %>% .coalesce_tourney_date()
+  end_dates <- dates %>% map_chr(~.str_replace_tourney_date(.x, 'end')) %>% .coalesce_tourney_date()
+  is_multi_day <- dates %>% str_detect('[-]')
+  
+  prizes <- prizes %>% str_remove_all('[$,]') %>% as.integer()
+  n_participant <- participants %>% str_remove('\\s+teams') %>% as.integer()
+
+  rgx_no_page <- ' \\(page does not exist\\)'
+  first_place_has_page <- first_place_names %>% str_detect(rgx_no_page)
+  second_place_has_page <- second_place_names %>% str_detect(rgx_no_page)
+  first_place_names <- first_place_names %>% str_remove(rgx_no_page)
+  second_place_names <- second_place_names %>% str_remove(rgx_no_page)
+
+  first_place_abbrvs <- first_place_abbrvs %>% .clean_abbrv()
+  second_place_abbrvs <- second_place_abbrvs %>% .clean_abbrv()
+
   tourneys <-
     tibble(
       name = names,
       url = sprintf('https://liquipedia.net%s', links),
-      date = dates,
+      start_date = start_dates,
+      end_date = end_dates,
+      is_multi_day = is_multi_day,
       prize = prizes,
+      region = regions,
       location = locations,
-      n_participants = participants %>% str_remove('\\s+teams') %>% as.integer(),
+      n_participants = n_participant,
+      first_place = first_place_names,
+      second_place = second_place_names,
       first_place_abbrv = first_place_abbrvs,
       second_place_abbrv = second_place_abbrvs,
-      first_place_name = first_place_names,
-      second_place_name = second_place_names
+      first_place_has_page = first_place_has_page,
+      second_place_has_page = second_place_has_page
     )
   tourneys
 }
@@ -71,7 +152,7 @@ tourney_urls <- tibble(
 possibly_scrape_tournament <- possibly(scrape_tournament, otherwise = tibble(), quiet = FALSE)
 tourneys <- tourney_urls %>% 
   mutate(tourney = map(url, possibly_scrape_tournament))
-
+tourneys
 all_tourneys <- tourneys %>%
   select(tier, tourney) %>% 
   unnest(tourney)
